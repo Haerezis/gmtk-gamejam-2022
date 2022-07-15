230fc028995e6e1020199074a4e911e17dd30d49
CatboyCodes <shamimur2010@gmail.com>
2022-07-15 12:05:24 -0700

    GUI
    Main Menu

---
+++ ui/Quit.gd
@@ -0,0 +1,8 @@
+extends Button
+
+
+func _ready():
+	pass
+
+func _on_Quit_button_down():
+	get_tree().quit()
---
+++ ui/Start.gd
@@ -0,0 +1,6 @@
+extends Button
+
+onready var node = get_node("/root/Main")
+
+func _on_Start_pressed():
+	node.hide() # Replace with function body.
