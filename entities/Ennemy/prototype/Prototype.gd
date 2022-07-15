ec356ef1bffe06068a2c48fff0ba2a766c65d2a3
MemokingMH <85684023+MemokingMH@users.noreply.github.com>
2022-07-15 23:33:18 +0300

    enemy

--- entities/Ennemy/prototype/Prototype.gd
+++ entities/Ennemy/prototype/Prototype.gd
@@ -2,8 +2,20 @@ extends KinematicBody2D
 
 var hp = 10
 
+var direction = -1
+
 func _ready(): 
 	$Hurtbox.connect("damage", self, "get_hit")
+	
+	$Timer.connect("timeout", self, "switchState")
+
+func _process(delta):
+	move_and_slide(Vector2(5 * direction, 0) / delta)
+
+func switchState():
+	direction = direction * -1
+	print("switch")
+	$Timer.start()
 
 func get_hit():
 	print("oof" + String(hp))
