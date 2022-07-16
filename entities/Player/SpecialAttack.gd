abd24bb85f33f7ff9d3cee68b9439a355428765f
Kuntz Thomas <thomaskuntz@sharedmemory.fr>
2022-07-17 00:28:25 +0200

    tmp

--- entities/Player/SpecialAttack.gd
+++ entities/Player/SpecialAttack.gd
@@ -1,10 +1,15 @@
 extends Node
 
 var player
+var d6_scene = preload("res://entities/D6 Power/D6.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_parent()
+
+func _input(event):
+	if event.is_action_pressed("shoot"):
+		$ChipThrower.shoot()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
