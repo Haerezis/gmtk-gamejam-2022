b71617b75a0804c5822b2a863ad56a2e20518c32
Kuntz Thomas <thomaskuntz@sharedmemory.fr>
2022-07-17 00:49:40 +0200

    tmp

--- entities/Player/SpecialAttack.gd
+++ entities/Player/SpecialAttack.gd
@@ -1,17 +1,27 @@
-extends Node
+extends Node2D
+
+export(Vector2) var throw_force
+export(NodePath) var camera
 
 var player
 var d6_scene = preload("res://entities/D6 Power/D6.tscn")
+var can_throw_d6 = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_parent()
+	randomize()
 
 func _input(event):
-	if event.is_action_pressed("shoot"):
-		$ChipThrower.shoot()
+	if event.is_action_pressed("special_attack"):
+		throw_d6()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func throw_d6():
+	var value = randi() % 6 + 1
+	var d6 : RigidBody2D = d6_scene.instance()
+	d6.global_position = global_position
+	d6.init(value, get_node(camera))
+	
+	get_tree().current_scene.add_child(d6)
+	d6.apply_central_impulse(throw_force)
