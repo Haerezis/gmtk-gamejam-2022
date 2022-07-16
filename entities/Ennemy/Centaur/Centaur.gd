5ab842da6829bb20dd360c4d43b859e04c6f9aee
Thomas Kuntz <thomaskuntz67@gmail.com>
2022-07-17 00:05:27 +0200

    Update Centaur enemy, add platform moving/vanishing

--- entities/Ennemy/Centaur/Centaur.gd
+++ entities/Ennemy/Centaur/Centaur.gd
@@ -2,82 +2,116 @@ extends KinematicBody2D
 
 var hp = 10
 
-enum {IDLE, MOVE, CHANGE, CHARGE, DASH}
-const SPEED = 200
+enum {CLOSE, IDLE, FOLLOW, CHARGE, DASH, MELEE}
+const SPEED = 60
 
-var currentState = MOVE
+var currentState = CLOSE
 var direction = Vector2.RIGHT
 
 onready var animPlayer = get_node("AnimationPlayer")
 
-export var maxDistance = 400
+var target 
+
+var dashing = false
 
 func _ready():
+	randomize()
 	
 	$Hurtbox.connect("damage", self, "get_hit")
 	$IFrameTimer.connect("timeout", self, "breakInvincible")
+	$TriggerArea.connect("area_entered", self, "seenPlayer")
+	$TriggerArea.connect("area_exited", self, "lostPlayer")
+	$TriggerArea.connect("body_exited", self, "lostPlayer")
+	$MeleeRange.connect("area_entered", self, "playerMeleeRange")
 
 func _process(delta):
-	if checkPlayer() and not charging:
-		currentState = CHARGE
-	
 	match currentState:
+		CLOSE:
+			pass
 		IDLE:
 			idle()
-		MOVE:
-			move(delta)
-		CHANGE:
-			newDirection()
+		FOLLOW:
+			follow()
 		CHARGE:
 			charge()
 		DASH:
-			dash(delta)
-	
-	move_and_slide(Vector2(0, 400))
+			dash()
+		MELEE:
+			melee()
 
-func randomise(array : Array):
-	array.shuffle()
-	return array.front()
+func seenPlayer(p):
+	target = p
+	currentState = IDLE
 
 func idle():
-	animPlayer.play("IDLE")
-	yield(get_tree().create_timer(3.0), "timeout")
-	
-	currentState = MOVE
+	if randf() < 0.7:
+		currentState = FOLLOW
+	else:
+		pass
 
-func move(delta):
-	if not charging:
-		for i in 3:
-			position += direction * SPEED * delta / 2
-			animPlayer.play("MOVE")
-		
-		currentState = CHANGE
+func follow():
+	changeFlip()
+	move_and_slide(getPlayerRotation() * SPEED)
+	if randf() < 0.005:
+		currentState = CHARGE
+	else:
+		pass
 
-func newDirection():
-	direction = randomise([Vector2.RIGHT, Vector2.LEFT])
-	currentState = IDLE
-	
-	$RayCast2D.cast_to = direction * 400
-
-var charging = false
-export var chargeTime = 2.0
-
+var chargeOnce = false
 func charge():
-	charging = true
-	animPlayer.play("CHARGE")
-	yield(get_tree().create_timer(chargeTime), "timeout")
-	currentState = DASH
+	if not chargeOnce:
+		print("charge")
+		chargeOnce = true
+		animPlayer.play("CHARGE")
+		yield(get_tree().create_timer(1.0), "timeout")
+		currentState = DASH
 
-func dash(delta):
+var a = 0.05
+func dash():
 	
-	for i in 3:
-		position += direction * SPEED * delta 
+	move_and_slide(-direction * SPEED * 20)
+	dashing = true
+	currentState = IDLE 
 	
-	currentState = IDLE
-	charging = false
+	if randf() < a:
+		print("leftdash")
+		currentState = FOLLOW
+		dashing = false
+		chargeOnce = false
+		a = 0.05
+	else:
+		a += 0.05
+		currentState = DASH
+		print("dash")
 
-func checkPlayer():
-	return $RayCast2D.is_colliding()
+func melee():
+	animPlayer.play("MELEE")
+
+func playerMeleeRange():
+	currentState = MELEE
+
+func getPlayerRotation():
+	if not target == null:
+		if target.global_position.x - global_position.x < 0:
+			return Vector2.LEFT
+			direction = Vector2.LEFT
+		else:
+			return Vector2.RIGHT
+			direction = Vector2.RIGHT
+
+func changeFlip():
+	if getPlayerRotation() == Vector2.RIGHT:
+		$Sprite.flip_h = false
+		$Fist.position = Vector2(60, 0)
+	else:
+		$Sprite.flip_h = true
+		$Fist.position = Vector2(-60, 0)
+
+func lostPlayer(a):
+	print("lost player")
+	
+	currentState = CLOSE
+	target = null
 
 var invincible = false
 export var iframeTime = 1.0
