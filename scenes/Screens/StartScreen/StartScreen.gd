ee9188a2fb7d5108a497afd09c4af3e5e39d841e
seanG <seang@gmail.com>
2022-07-17 15:20:01 +0100

    Added start animation

--- scenes/Screens/StartScreen/StartScreen.gd
+++ scenes/Screens/StartScreen/StartScreen.gd
@@ -8,10 +8,14 @@ func _ready():
 	$Buttons/ExitButton.connect("pressed", self, "exitGame")
 
 func startLevel():
-	get_tree().change_scene("res://scenes/level_1/level_1.tscn")
+	$AnimatedSprite.visible = true
+	$AnimatedSprite.play("default", true)
 
 func creditsScreen():
 	get_tree().change_scene("res://scenes/Screens/CreditsScreen/CreditsScreen.tscn")
 
 func exitGame():
 	get_tree().quit()
+
+func transitionToLevel():
+	get_tree().change_scene("res://scenes/level_1/level_1.tscn")
