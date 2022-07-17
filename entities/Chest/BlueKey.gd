4366cdd16b6ff79c652608a48b6a3b92376c24ee
Kuntz Thomas <thomaskuntz@sharedmemory.fr>
2022-07-17 19:32:54 +0200

    Update BlueKey.gd

--- entities/Chest/BlueKey.gd
+++ entities/Chest/BlueKey.gd
@@ -7,10 +7,7 @@ func _ready():
 func trigger():
 	print("blue key")
 	$AudioStreamPlayer.play()
-	yield(get_tree().create_timer(0.6), "timeout")
 	get_parent().get_node("Chest").blueKey = true
-	print(get_parent().get_node("Chest").blueKey)
-	print(get_parent().get_node("Chest").greenKey)
 	get_parent().get_node("HUD").get_node("MarginContainer/HBoxContainer/bluekey").key_grabbed()
 	queue_free()
 
