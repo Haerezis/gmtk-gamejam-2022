439b24f2ab096c852921dd25013b52adc1903da0
Kuntz Thomas <thomaskuntz@sharedmemory.fr>
2022-07-17 14:15:31 +0200

    add water to level1

--- entities/Platforms/water.gd
+++ entities/Platforms/water.gd
@@ -4,20 +4,20 @@ onready var particles = preload("waterParticles.tscn")
 var prev_particles = []
 
 func _on_Area2D_area_entered(area):
-    var particlesInstance = particles.instance()
-    
-    add_child(particlesInstance)
-    particlesInstance.global_position = area.global_position
-    particlesInstance.set_emitting(true)
-    
-    prev_particles.append(particlesInstance)
+	var particlesInstance = particles.instance()
+	
+	add_child(particlesInstance)
+	particlesInstance.global_position = area.global_position
+	particlesInstance.set_emitting(true)
+	
+	prev_particles.append(particlesInstance)
 
 
 func _physics_process(_d):
-    var i = 0
-    for particle in prev_particles:
-        if not particle.is_emitting():
-            particle.queue_free()
-            prev_particles.remove(i)
-            
-        i+=1
+	var i = 0
+	for particle in prev_particles:
+		if not particle.is_emitting():
+			particle.queue_free()
+			prev_particles.remove(i)
+			
+		i+=1
