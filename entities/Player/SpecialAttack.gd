275e8a1ddf0ceb3fd0c816679ac41ccd0a8e4277
Kuntz Thomas <thomaskuntz@sharedmemory.fr>
2022-07-17 10:06:10 +0200

    Finish special attack

--- entities/Player/SpecialAttack.gd
+++ entities/Player/SpecialAttack.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 export(Vector2) var throw_force
-export(NodePath) var camera
 
 var player
+var camera
 var d6_scene = preload("res://entities/D6 Power/D6.tscn")
 var can_throw_d6 = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_parent()
+	camera = get_tree().current_scene.get_node("Camera2D")
 	randomize()
 
 func _input(event):
@@ -21,7 +22,7 @@ func throw_d6():
 	var value = randi() % 6 + 1
 	var d6 : RigidBody2D = d6_scene.instance()
 	d6.global_position = global_position
-	d6.init(value, get_node(camera))
+	d6.init(value, camera)
 	
 	get_tree().current_scene.add_child(d6)
 	d6.apply_central_impulse(throw_force)
