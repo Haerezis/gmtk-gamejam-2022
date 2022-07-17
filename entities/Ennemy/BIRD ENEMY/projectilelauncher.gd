51968423787e75923103a18264f683c529257787
saibathebest <89657472+siba1426@users.noreply.github.com>
2022-07-17 17:45:55 +0300

    better bird enemy

--- entities/Ennemy/BIRD ENEMY/projectilelauncher.gd
+++ entities/Ennemy/BIRD ENEMY/projectilelauncher.gd
@@ -5,7 +5,7 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var projectile = preload('res://entities/enemy projectile/enemy projectile.tscn')
-
+export var damage = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,6 +13,7 @@ func bullet(dir):
 	var newprojectile = projectile.instance()
 	newprojectile.dir = dir
 	newprojectile.scale = newprojectile.scale *5
+	newprojectile.attack = damage
 	newprojectile.position = self.position
 	owner.add_child(newprojectile)
 
