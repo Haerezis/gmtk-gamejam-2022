506cc3b5dfc408d91eddca8e30348a3dc5304641
saibathebest <89657472+siba1426@users.noreply.github.com>
2022-07-17 18:05:17 +0300

    made a key hud item and made the bird enemy better and added enemy sprite (#32)
    * made a key hud item
    
    * better bird enemy
    
    Co-authored-by: Kuntz Thomas <thomaskuntz@sharedmemory.fr>

--- entities/Chest/GreenKey.gd
+++ entities/Chest/GreenKey.gd
@@ -7,4 +7,9 @@ func _ready():
 func trigger():
 	print("green key")
 	get_parent().get_node("Chest").greenKey = true
+	get_parent().get_node("HUD").get_node("MarginContainer/HBoxContainer/Greenkey").blue_keyy_grabbed()
 	queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	trigger()
