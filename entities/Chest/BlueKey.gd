18066c42371d77e21618b3b3a47034293b29ada1
saibathebest <89657472+siba1426@users.noreply.github.com>
2022-07-17 17:07:54 +0300

    made a key hud item

--- entities/Chest/BlueKey.gd
+++ entities/Chest/BlueKey.gd
@@ -4,7 +4,13 @@ func _ready():
 	$Area2D.connect("area_entered", self, "trigger")
 	$Area2D.connect("body_entered", self, "trigger")
 
+
 func trigger():
 	print("blue key")
 	get_parent().get_node("Chest").blueKey = true
+	get_parent().get_node("HUD").get_node("MarginContainer/HBoxContainer/bluekey").blue_keyy_grabbed()
 	queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	trigger()
